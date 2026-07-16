5f373824d0130955c2fbb1b52b58374282590d41
jirofg <mechcromancer@gmail.com>
2026-07-15 19:11:23 -0600

    [FEAT] Validaciones de la entidad producto

--- internal/domain/entities/product.go
+++ internal/domain/entities/product.go
@@ -1,11 +1,26 @@
 package entities
 
+import "errors"
+
 type Product struct {
 	ID        string
 	Name      string
 	Price     float64
 	Stock     int32
 	CreatedAt string
+}
+
+func (p *Product) Validate() error {
+	if p.Name == "" {
+		return errors.New("Name requerido")
+	}
+	if p.Price <= 0 {
+		return errors.New("Price no puede ser menor o igual a cero")
+	}
+	if p.Stock < 0 {
+		return errors.New("Stock no puede ser menor o igual a cero")
+	}
+	return nil
 }
 
 type UpdateProduct struct {
